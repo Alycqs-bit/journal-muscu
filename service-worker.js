a4af23c8988b9b71072909b0afda8587d96dc854
Alix <alycqs@gmail.com>
2026-08-16 17:22:11 +0200

    Service worker: strategie reseau d'abord (cache en secours hors-ligne uniquement)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "journal-muscu-v4";
+const CACHE_NAME = "journal-muscu-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -26,5 +26,14 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
