d1c8b04c7830389e90e3246f5f5e51ea2fc92202
Alix <alycqs@gmail.com>
2026-08-16 17:01:38 +0200

    Service worker : skipWaiting + clients.claim pour mise a jour immediate

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = "journal-muscu-v2";
+const CACHE_NAME = "journal-muscu-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./app.js"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
